62bdce8441ea1f8cf247200e4e46ca59cf2a86c0
harryho <harry.ho_long@yahoo.com>
2018-03-10 17:27:13 +1100

    Update html and server.js

--- basic-02/code/server.js
+++ basic-02/code/server.js
@@ -1,20 +1,46 @@
-const http = require('http');
-const { URL, URLSearchParams } = require('url');
+const http = require("http");
+var fs = require("fs");
 
-const hostname = 'test.nodeapp.hh';
+const { URL, URLSearchParams } = require("url");
+
+const hostname = "test.nodeapp.hh";
 const port = 4000;
 
 const server = http.createServer((request, response) => {
+  request.on("error", err => {
+    console.error(err);
+    res.statusCode = 400;
+    res.end();
+  });
+  response.on("error", err => {
+    console.error(err);
+  });
 
   const name = new URLSearchParams(
-      request.url.substring(
-          request.url.indexOf("?"))).get('name');
+    request.url.substring(request.url.indexOf("?"))
+  ).get("name");
 
-  console.log(name)
+  if (request.url.startsWith("/hi?")) {
+    response.statusCode = 200; //OK
+    response.setHeader("Content-Type", "text/plain");
+    response.end(`Hello ${name}`);
+  }
 
-  response.statusCode = 200;//OK
-  response.setHeader('Content-Type', 'text/plain');
-  response.end(`Hello ${name}`);
+  else if (request.url.startsWith("/index.html?")) {
+    console.log(name, request.url);
+    fs.readFile("index.html", "utf8", (err, data) => {
+      const result = data.replace(/\$name\$/g, name);
+
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(result);
+      response.end();
+    });
+  }
+  else {
+    response.writeHead(404, { "Content-Type": "text/plain" });
+    response.write('page not found');
+    response.end();
+  }
 });
 
 server.listen(port, hostname, () => {
