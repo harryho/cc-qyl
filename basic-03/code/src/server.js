4f8e26112b249aa4f9865d7eb874dd0f67abfe6e
harryho <harry.ho_long@yahoo.com>
2018-03-12 23:41:34 +1100

    Incomplete sever.js

--- basic-03/code/src/server.js
+++ basic-03/code/src/server.js
@@ -9,42 +9,97 @@ class Customer {
     this.mobile = mobile;
     this.credit = credit;
   }
+
+  // get Name= ()=>this.name;
 }
 
 class CustomerService {
   constructor() {
     // Initialize a list of customer
     this.customers = [
-      new Customer("Mr", "John", "abc@john.com", "040370402", 36),
-      new Customer("Mr", "Ming", "abc@john.com", "040370402", 36),
-      new Customer("Mr", "Jason", "abc@john.com", "040370402", 36),
-      new Customer("Ms", "Lyn", "abc@john.com", "040370402", 36),
-      new Customer("Mrs", "Brown", "abc@john.com", "040370402", 36)
+      new Customer("Mr", "John", "abc@john.com", "040370402", 536),
+      new Customer("Mr", "Ming", "abc@john.com", "040370102", 436),
+      new Customer("Mr", "Jason", "abc@john.com", "040371402", 936),
+      new Customer("Ms", "Lyn", "abc@john.com", "04037002", 366),
+      new Customer("Mrs", "Brown", "abc@john.com", "040370402", 236),
+      new Customer("Mrs", "Brown", "abc@john.com", "040370402", 1636)
     ];
   }
-  // method 1 getCustomers
-  // Return all customres
-  getCustomers = () => {
-    return this.customers;
+
+  isNullOrEmpty = test =>
+    test === undefined || test === null || test.trim().length === 0;
+
+  capitalize = s => s.charAt(0).toUpperCase() + s.slice(1);
+
+  compare = (a, b) => {
+    if (isNaN(a) && typeof a === "string") return a.localeCompare(b);
+    else if (!isNaN(a)) return a - b;
   };
-  // TODO
-  searchCustomers = () => {};
+
+  sortCustomers = (list, sortByFields, isAscending) => {
+    if (sortByFields)
+      list.sort((a, b) => {
+        let idx = 0;
+        while (this.compare(a[sortByFields[idx]], b[sortByFields[idx]]) === 0) {
+          idx++;
+        }
+        return isAscending
+          ? this.compare(a[sortByFields[idx]], b[sortByFields[idx]])
+          : this.compare(b[sortByFields[idx]], a[sortByFields[idx]]);
+      });
+  };
+  getCustomers = () => this.customers;
 
   // TODO
-  renderCustomers = () => {
+  searchCustomers = filters => {
+    if (filters) {
+      return this.customers.filter(customer =>
+        Object.keys(filters).every(
+          key =>
+            this.isNullOrEmpty(filters[key])
+              ? true
+              : customer[key].toLowerCase().includes(filters[key].toLowerCase())
+        )
+      );
+    }
+    return this.customers;
+  };
+
+  // TODO
+  renderCustomers = (headers, customers) => {
     // TODO
     // Expect result
     // Title Name  Mobile       Email            Credit
     // Mr  | ABC  |   041235 |  abc@email.com | 23
-    let result = "";
-    const splitor = " | ";
-    for (var cust of this.customers) {
-      result +=
-        [cust.title, cust.name, cust.email, cust.mobile, cust.credit].join(
-          splitor
-        ) + "\n";
+    if (headers && customers) {
+      let result = "";
+      let head = "";
+      const splitor = " | ";
+
+      result += headers.map(head => this.capitalize(head)).join(splitor) + "\n";
+
+      result +=  customers.map(cust=>
+       headers.map(h => cust[h]).join(splitor)).join("\n");
+      
+      console.log(result);
     }
-    console.log(result);
+  };
+
+  //TODO 
+  highlightMatchedFilter = filters => {
+    if (filters) {
+      return this.customers.filter(customer =>
+        Object.keys(filters).every(
+          key =>
+            this.isNullOrEmpty(filters[key])
+              ? true
+              : customer[key]
+                  .toLowerCase()
+                  .includes(filters[key].toLowerCase())
+        )
+      );
+    }
+    return this.customers;
   };
 }
 
@@ -53,30 +108,27 @@ const App = () => {
     run: () => {
       console.log(" App loading ... ");
     },
-    search: () => {},
-    render: () => {
-      csSeverice = new CustomerService();
-
-      //TODO
-      const listHeader = ["name", "mobile", "credit", "email"];
-      csSeverice.renderCustomers(listHeader);
-
-      // TODO
-      const filters = {
-        name: "a",
-        email: "b",
-        mobile: "1"
-      };
-      // TODO
-      // Expect any customers whoes name contains the filter's
-      // name, etc.
+    getAllCustomers: () => {
+      const csSeverice = new CustomerService();
+      return csSeverice.getCustomers();
+    },
+    search: filters => {
+      const csSeverice = new CustomerService();
       const customerList = csSeverice.searchCustomers(filters);
-      console.log(customerList);
+      const listHeader = ["name", "mobile", "credit", "email", "mobile"];
+      csSeverice.renderCustomers(listHeader, customerList);
+    },
+    sort: (customers, sortByFields, isAscending) => {
+      const csSeverice = new CustomerService();
+      csSeverice.sortCustomers(customers, sortByFields, isAscending);
+      const listHeader = ["name", "mobile", "credit", "email"];
+      csSeverice.renderCustomers(listHeader, customers);
+    },
+    render: () => {
+      const csSeverice = new CustomerService();
+      const listHeader = ["name", "mobile", "credit", "email"];
+      csSeverice.renderCustomers(listHeader, csSeverice.getCustomers());
 
-      // TODO
-      // highlight matched content
-      // Expect fitler name contains "a"
-      // J<highlight>a</highlight>son
     }
   };
 };
@@ -84,5 +136,24 @@ const App = () => {
 (app => {
   app.run();
 
+  console.log(" Render ... \n");
   app.render();
+
+  console.log(" Search with filters ... \n");
+  // TODO
+  const filters = {
+    name: "n",
+    email: "b",
+    mobile: "1"
+  };
+  app.search(filters);
+
+  console.log(" Sort by descending... \n");
+
+  sortByFields = ["name", "email", "credit"];
+  isAscending = false;
+
+  const customers = app.getAllCustomers();
+
+  app.sort(customers, sortByFields, isAscending);
 })(App());
