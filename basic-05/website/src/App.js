7e9b41ce175a7572eef37c2ea3989c1c0ef74b45
harryho <harry.ho_long@yahoo.com>
2018-03-29 09:37:15 +1100

    update basic-05 with pagination implementation

--- basic-05/website/src/App.js
+++ basic-05/website/src/App.js
@@ -30,29 +30,6 @@ export const App = () => {
       searchData : (filter, headers)=> {
         const csSeverice = new CustomerService();
         csSeverice.searchCustomers( filter, headers, (customerList, headers) => {render(customerList, headers)})
-      },
-      // sort: (customers, sortByFields, isAscending) => {
-      //   const csSeverice = new CustomerService();
-      //   csSeverice.sortCustomers(customers, sortByFields, isAscending);
-      //   const listHeader = ["name", "mobile", "credit", "email"];
-      //   csSeverice.renderCustomers(listHeader, customers);
-      // },
-      // render:(customers, listHeader) => {
-      //   // const csSeverice = new CustomerService();
-      //   // const listHeader = ["name", "mobile", "credit", "email"];
-      //   // csSeverice.renderCustomers(listHeader, csSeverice.getCustomers());  
-      //   const msg = document.getElementById("message");
-      //   msg.innerText = "";
-      //   const tbl = document.getElementById("table");
-      //   tbl.appendChild(RenderHelper.renderDataTable(customers, listHeader ))
-      // },
-    
-      // Hlight matched content
-      // E.g. If we expect customer's name contains "a"
-      // the search result will looks like J<a>son, and a is rendered in red color
-      // highlightMatchedFilter: filters => {
-      //   const csSeverice = new CustomerService();
-      //   csSeverice.highlightMatchedFilter(filters);
-      // }
+      }
     };
   };
--- basic-05/website/src/RenderHelper.js
+++ basic-05/website/src/RenderHelper.js
@@ -1,5 +1,7 @@
 import { CustomerService } from "./CustomerService";
 
+const PAGE_SIZE = 2;
+
 const createTable = () => {
   const t = document.createElement("table");
   t.setAttribute(
@@ -29,35 +31,123 @@ const createTableData = () => {
   return td;
 };
 
+const createPageLink = ( pageStepElm )
 
-export function sortTable(container, data, headers, sortingHeader) {
+const createPagination = (
+  container,
+  data,
+  headers,
+  sortingHeader,
+  pagination
+) => {
+  const pages = document.createElement("div");
+  const pageNum = pagination.pageNum || 1;
+  const totalPages = pagination.totalPages;
+
+  let pageCount = 1;
+  while (pageCount <= totalPages) {
+    if (pageCount === 1) {
+      // console.log(" pagenum ", pageNum, " page count", pageCount)
+      if (1 !== pageNum) {
+        const firstLink = document.createElement("a");
+        firstLink.innerText = " << ";
+          firstLink.setAttribute("style", "color:red; font-weight:bold;");
+        firstLink.onclick = function() {
+          sortTable(container, data, headers, sortingHeader, {
+            ...pagination,
+            pageNum: 1
+          });
+        };
+        pages.appendChild(firstLink);
+      } else {
+        const first = document.createElement("span");
+        first.innerText = " << ";
+        pages.appendChild(first);
+      }
+    }
+
+    if (pageCount !== pageNum) {
+      const pageLink = document.createElement("a");
+      const pNum = pageCount;
+      pageLink.innerText = " " + pageCount + " ";
+      pageLink.setAttribute("style", "color:red; font-weight:bold;");
+      pageLink.onclick = function() {
+        sortTable(container, data, headers, sortingHeader, {
+          ...pagination,
+          pageNum: pNum
+        });
+      };
+      pages.appendChild(pageLink);
+    } else {
+      const page = document.createElement("span");
+      page.innerText = " " + pageCount + " ";
+      pages.appendChild(page);
+    }
+
+    if (pageCount === totalPages) {
+      if (totalPages !== pageNum) {
+        const endLink = document.createElement("a");
+        endLink.innerText = " >> ";
+        endLink.setAttribute("style", "color:red; font-weight:bold;");
+        endLink.onclick = function() {
+          sortTable(container, data, headers, sortingHeader, {
+            ...pagination,
+            pageNum: totalPages
+          });
+        };
+        pages.appendChild(endLink);
+      } else {
+        const end = document.createElement("span");
+        end.innerText = " >> ";
+        pages.appendChild(end);
+      }
+    }
+    pageCount++;
+  }
+  return pages;
+};
+
+// pagination {
+//  pageNum: 1 ,  // by defaut
+//  pageSize: 2, // e.g. 2 records per page -- pageSize needs to be setup
+// totalPages: 2 // total 2 pages
+// }
+export function sortTable(container, data, headers, sortingHeader, pagination) {
   console.log("sorting ...");
   const sc = new CustomerService();
   const sorting = document.getElementById(sortingHeader);
-  let sortedData = [];
+
   if (!sorting || sorting.innerText === "arrow_upward") {
-   sc.sortCustomers(data, [sortingHeader], true);
+    sc.sortCustomers(data, [sortingHeader], true);
   } else sc.sortCustomers(data, [sortingHeader], false);
 
-  renderDataTable(container, data, headers, sortingHeader);
+  renderDataTable(container, data, headers, sortingHeader, pagination);
 }
 // sortingHeader
-export function renderDataTable(container, data, headers, sortingHeader) {
+export function renderDataTable(
+  container,
+  data,
+  headers,
+  sortingHeader,
+  pagination
+) {
   const table = createTable();
-  const sc= new CustomerService()
+  const sc = new CustomerService();
+
+  // Table header
   if (headers) {
     const thead = createTableHeader();
     table.appendChild(thead);
 
     headers.forEach(element => {
       const th = createTableHeaderCell();
-      th.innerText = sc.capitalize( element)
+      th.innerText = sc.capitalize(element);
       let sorting = "";
       let direction = "";
 
       if (element === sortingHeader) {
         sorting = document.getElementById(element);
-        direction = sorting? sorting.innerText:"arrow_downward";
+        direction = sorting ? sorting.innerText : "arrow_downward";
         console.log(direction);
       }
 
@@ -71,7 +161,10 @@ export function renderDataTable(container, data, headers, sortingHeader) {
         directionIcon = document.createElement("i");
         directionIcon.setAttribute("id", element);
         directionIcon.setAttribute("class", "material-icons md-18");
-        directionIcon.setAttribute("style", "padding-left:1px; font-size: medium;");
+        directionIcon.setAttribute(
+          "style",
+          "padding-left:1px; font-size: medium;"
+        );
         directionIcon.innerText =
           direction === "arrow_upward" ? "arrow_downward" : "arrow_upward";
         th.appendChild(directionIcon);
@@ -80,17 +173,32 @@ export function renderDataTable(container, data, headers, sortingHeader) {
       thead.appendChild(th);
     });
   }
-
+  // Table body
   if (data && data.length > 0) {
-    // if (sorting && sorting.length > 0 ){
-    //   const sc = CustomerService
-    //   data = sc.sortCustomers (data, sortByFields, sorting[0].asc)
-    // }
+    if (!pagination) {
+      // if (data.length>0 ){
+      const pageSize = PAGE_SIZE;
+      const totalPages = Math.ceil(data.length / pageSize);
+
+      pagination = { pageSize, totalPages, pageNum: 1 };
+      // table.appendChild(pageElm)
+      // }
+    }
 
     const tbody = createTableBody();
     table.appendChild(tbody);
 
-    data.forEach(element => {
+    let contentList = data;
+
+    if (pagination) {
+      // const pageNum = pagination.pageNum || 1;
+      contentList = data.slice(
+        (pagination.pageNum - 1) * pagination.pageSize,
+        pagination.pageNum * pagination.pageSize
+      );
+    }
+
+    contentList.forEach(element => {
       const tr = createTableRow();
       for (const key in element) {
         const td = createTableData();
@@ -99,10 +207,27 @@ export function renderDataTable(container, data, headers, sortingHeader) {
       }
       tbody.appendChild(tr);
     });
+
+    // Pagination
+
+    if (contentList.length > 0) {
+      // pagination.pageSize = pagination.pageSize;// ||PAGE_SIZE;
+      // pagination.totalPages = Math.ceil(data.length/pagination.pageSize)
+      const pageElm = createPagination(
+        container,
+        data,
+        headers,
+        sortingHeader,
+        pagination
+      );
+      console.log(pagination);
+      table.appendChild(pageElm);
+    }
   }
+
   // return table;
-  container.children && container.children.length>0 && container.removeChild(container.childNodes[0])
-  container.appendChild(table)
+  container.children &&
+    container.children.length > 0 &&
+    container.removeChild(container.childNodes[0]);
+  container.appendChild(table);
 }
-
-
